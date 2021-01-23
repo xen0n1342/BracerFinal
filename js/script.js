4ce022a9f5c18e48ebbd8e056bdd658e05813ff4
Nick <bigbasspoll@gmail.com>
2021-01-23 03:48:02 +0300

    finalBracer

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
-    //slider
     $('.carousel__inner').slick({
         speed: 1200,
-        // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         autoplay: true,
         autoplaySpeed: 2000,
         fade: true,
@@ -18,13 +17,11 @@ $(document).ready(function(){
             }
         ]
     });
-    //tabs
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
           .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
-
     function toggleSlide(item){ 
         $(item).each(function(i) {
             $(this).on('click', function(e) {
@@ -34,28 +31,22 @@ $(document).ready(function(){
             })
         });
     };
-
     toggleSlide('.catalog-item__back');
     toggleSlide('.catalog-item__link');
-    //Modal
     $('[data-modal=consultation]').on('click', function(){
         $('.overlay, #consultation').fadeIn('slow');
     });
-
     $('.modal__close').on('click', function(){
         $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
     });
-
     $('.button_mini').on('click', function(){
         $('.overlay, #order').fadeIn('slow');
     });
-
     $('.button_mini').each(function(i){
         $(this).on('click', function(){
             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
         });
     });
-    //Validate
     function ValidateForms(form){
         $(form).validate({
             rules: {
@@ -86,8 +77,6 @@ $(document).ready(function(){
     ValidateForms('#consultation form');
     ValidateForms('#order form');
     $('input[name=phone]').mask ("(+7 (999) 999-99-99)");
-    //SendMessagesAjax
-    
     $('form').submit(function(e){
         e.preventDefault();
         $.ajax({
@@ -102,8 +91,6 @@ $(document).ready(function(){
         });
         return false;
     });
-    
-    //Smooth scroll and pageup
     $(window).scroll(function(){
         if($(this).scrollTop() > 1600){
             $('.pageup').fadeIn();
@@ -112,7 +99,6 @@ $(document).ready(function(){
             $('.pageup').fadeOut();
         }
     });
-
     function scroll (item){
         $(item).on('click', function() {
             let href = $(this).attr('href');
@@ -124,24 +110,20 @@ $(document).ready(function(){
     };
     scroll('a[href^="#down"');
     scroll('a[href^="#up"');
-    //AnimationWOW
     const wow = new WOW(
         {
-          boxClass:     'wow',      // animated element css class (default is wow)
-          animateClass: 'animated', // animation css class (default is animated)
-          offset:       0,          // distance to the element when triggering the animation (default is 0)
-          mobile:       true,       // trigger animations on mobile devices (default is true)
-          live:         true,       // act on asynchronously loaded content (default is true)
+          boxClass:     'wow', 
+          animateClass: 'animated',
+          offset:       0,    
+          mobile:       true,   
+          live:         true,
           callback:     function(box) {
-            // the callback is fired every time an animation is started
-            // the argument that is passed in is the DOM node being animated
           },
-          scrollContainer: null,    // optional scroll container selector, otherwise use window,
-          resetAnimation: false,     // reset animation on end (default is true)
+          scrollContainer: null,    
+          resetAnimation: false,    
         }
       );
-      wow.init();
-
+    wow.init();
     $(".footer__arrow").click(function() {
         $('.footer__arrow').toggleClass('footer__arrow_active');
         $('.footer__info').toggleClass('footer__info_active');
